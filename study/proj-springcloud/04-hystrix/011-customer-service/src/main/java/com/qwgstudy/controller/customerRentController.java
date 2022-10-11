be343f1f1b751bb9cecaa3500c7207985d4a04a9
16173 <1617325990@qq.com>
2022-10-11 17:03:22 +0800

    学习springcloud

--- study/proj-springcloud/04-hystrix/011-customer-service/src/main/java/com/qwgstudy/controller/customerRentController.java
+++ study/proj-springcloud/04-hystrix/011-customer-service/src/main/java/com/qwgstudy/controller/customerRentController.java
@@ -5,10 +5,13 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.annotation.Resource;
+
 @RestController
 public class customerRentController {
 
-    @Autowired
+//    @Autowired
+    @Resource
     private CustomerRentFeign customerRentFeign;
 
     @GetMapping("customerRent")
--- study/proj-springcloud/04-hystrix/04-feign-project/user-ServiceCenter/src/main/java/com/qwgstudy/controller/UserController.java
+++ study/proj-springcloud/04-hystrix/04-feign-project/user-ServiceCenter/src/main/java/com/qwgstudy/controller/UserController.java
@@ -3,13 +3,18 @@ package com.qwgstudy.controller;
 import com.qwgstudy.domain.Order;
 import com.qwgstudy.feign.UserOrderFeign;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.annotation.Resource;
+
 @RestController
 public class UserController {
-
-    @Autowired
+//
+//    @Autowired
+//    @Qualifier("userOrderFeign")
+    @Resource
     private UserOrderFeign userOrderFeign;
 
     @GetMapping("find")
